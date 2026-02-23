8d88d28260fab68c5ce2f07d195067fa67cd9951
Álvaro Álvarez <alvarez.redondo.alvaro@gmail.com>
2026-02-23 18:26:34 +0100

    fixes

--- tests/testthat/test_all.r
+++ tests/testthat/test_all.r
@@ -1,4 +1,7 @@
-source(here::here("tests", "testthat", "r", "test_setup_context.r"), echo = FALSE)
+source(
+  here::here("tests", "testthat", "r", "test_setup_context.r"),
+  echo = FALSE
+)
 
 testthat::test_dir(
   here::here("tests", "testthat", "r"),
