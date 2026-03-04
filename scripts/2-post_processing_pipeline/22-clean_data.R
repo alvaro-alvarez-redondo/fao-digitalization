da1274f1aa648454aec86fbe75d007b9f7ee5e33
Álvaro Álvarez Redondo <alvarez.redondo.alvaro@gmail.com>
2026-03-04 10:50:17 +0100

    Remove redundant clean/harmonize wrapper scripts

--- scripts/2-post_processing_pipeline/22-clean_data.R
+++
@@ -1,9 +0,0 @@
-# script: compatibility wrapper for clean stage functions
-# description: sources unified clean/harmonize implementation to preserve
-# legacy script path compatibility.
-
-source(here::here(
-  "scripts",
-  "2-post_processing_pipeline",
-  "22-clean_harmonize_data.R"
-), echo = FALSE)
